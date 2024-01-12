15962e52ea59f8325838aec31bb8225c6f417ec6
Justin <jdiaz189@calpoly.edu>
2024-01-11 22:59:19 -0800

    Ready to grade

--- lab2.py
+++ lab2.py
@@ -1,10 +1,49 @@
 def max_list_iter(int_list):
-    pass
 
+    if type(int_list) != list:
+        raise ValueError("Not a list")
+    
+    if not int_list:
+        return None
+    
+    high = int_list[0]
+
+    for i in int_list:
+        if i > high:
+            high = i
+    return high
+pass
 
 def reverse_rec(int_list):
-    pass
+     if type(int_list) != list:
+        raise ValueError("Not a list")
+    
+     if len(int_list) == 0:
+        return []
+     else: 
+         return [int_list[-1]] + reverse_rec(int_list[:-1])
+     
+pass
 
 
 def bin_search(target, low, high, int_list):
-    pass
+
+    if type(int_list) != list:
+        raise ValueError("Not a list")
+    
+    if len(int_list) == 0:
+        return None
+
+    if low <= high:
+        mid = (high + low) // 2
+        
+        if int_list[mid] == target:
+            return mid  
+        elif int_list[mid] < target:
+            return bin_search(target, mid + 1, high, int_list)
+        else:
+            return bin_search(target, low, mid - 1, int_list)
+    else:
+        return None  
+pass
+
--- test_student.py
+++ test_student.py
@@ -5,29 +5,29 @@ class TestLab2(unittest.TestCase):
 
     # Tests for max_list_iter
     def test_max_list_iter_custom1(self):
-        # Add your custom test case here
+        self.assertEqual(max_list_iter([1000, 0, 92, -34, 400]), 1000)
         pass
 
     def test_max_list_iter_custom2(self):
-        # Add your custom test case here
+        self.assertEqual(max_list_iter([-50, -34, 0]), 0)
         pass
 
     # Tests for reverse_rec
     def test_reverse_rec_custom1(self):
-        # Add your custom test case here
-        pass
+       self.assertEqual(reverse_rec([500, 23, 89, -10]), [-10, 89, 23, 500])
+       pass
 
     def test_reverse_rec_custom2(self):
-        # Add your custom test case here
+        self.assertEqual(reverse_rec([60]), [60])
         pass
 
     # Tests for bin_search
     def test_bin_search_custom1(self):
-        # Add your custom test case here
-        pass
+       self.assertIsNone(bin_search(8, 0, 4, [1, 2, 3, 4, 5]))
+       pass
 
     def test_bin_search_custom2(self):
-        # Add your custom test case here
+        self.assertEqual(bin_search(23, 0, 0, [23]), 0)
         pass
 
 if __name__ == '__main__':
